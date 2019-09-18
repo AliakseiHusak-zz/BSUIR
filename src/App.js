fd46391055f9dbe4d0d489de3d84bca824a59dce
Aleksei Husak <alhusak96@gmail.com>
2019-09-18 15:05:19 +0300

    LR-1 add contentPanel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/header/header";
+import ContentPanel from "./components/contentPanel/contentPanel";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ContentPanel />
     </div>
   );
 }
---
+++ src/components/contentPanel/contentPanel.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./contentPanel.css";
+
+const ContentPanel = () => (
+  <div className="contentPanel">
+    <div className="contentPanel__leftSide">left</div>
+    <div className="contentPanel_rightSide">right</div>
+  </div>
+);
+
+export default ContentPanel;
